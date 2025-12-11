8887eae12a69db2b18d309196f90166a403f3473
Juan Jose Castro <juanjosecastrogutierrez@gmail.com>
2025-12-11 00:00:09 -0500

    feat: Add address form and navigation components with styling and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { AddressFormComponent } from './address-form/address-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'address-form', component: AddressFormComponent }];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { NavigationComponent } from "./navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DashboardComponent],
+  imports: [RouterOutlet, DashboardComponent, NavigationComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
